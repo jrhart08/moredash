e7962a6731160ea2eef78eac09d0f18c60e8525d
Joseph Hart <josephrhart@gmail.com>
2021-05-18 19:15:48 -0500

    adding spec file linting overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,30 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      project: './tsconfig.json',
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'airbnb-typescript/base',
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+  overrides: [
+    {
+      files: ['src/**/*.spec.ts'],
+      rules: {
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-underscore-dangle': 'off',
+        '@typescript-eslint/naming-convention': 'off',
+      },
     },
-    plugins: [
-      '@typescript-eslint',
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:jest/recommended',
-      'airbnb-typescript/base',
-    ],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
-    },
+  ],
 };
