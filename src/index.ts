1ae90aa896342a74e53f03658c06c303d32d81c1
Eric Siebeneich <eric.siebeneich@external.iherb.com>
2021-09-23 16:16:41 -0500

    Added test to ensure exports are exported

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 /* istanbul ignore file */
-export { default as partitionObject } from './partitionObject';
-export { default as partitionObjectBy } from './partitionObjectBy';
-export { default as trimObject } from './trimObject';
+export { default as camelCaseObject } from './camelCaseObject';
+export { default as delay } from './delay';
+export { default as lazy } from './lazy';
+export { default as mapKeysRecursive } from './mapKeysRecursive';
 export { default as move } from './move';
 export { default as moveWhere } from './moveWhere';
-export { default as mapKeysRecursive } from './mapKeysRecursive';
-export { default as camelCaseObject } from './camelCaseObject';
+export { default as partitionObject } from './partitionObject';
+export { default as partitionObjectBy } from './partitionObjectBy';
+export { default as pickDeep } from './pickDeep';
+export { default as project } from './project';
+export { default as sleep } from './sleep';
+export { default as trimObject } from './trimObject';
+export { default as withKey } from './withKey';
--- src/promise/index.ts
+++
@@ -1,3 +0,0 @@
-/* istanbul ignore file */
-export { default as sleep } from './sleep';
-export { default as delay } from './delay';
