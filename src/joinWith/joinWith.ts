3c21419ad10170d686f13c82bc797b3c71b18128
Eric Siebeneich <eric.siebeneich@external.iherb.com>
2021-09-28 11:58:45 -0500

    added joinWith

---
+++ src/joinWith/joinWith.ts
@@ -0,0 +1,17 @@
+import {
+  flatMap,
+} from 'lodash/fp';
+import withKey from '../withKey';
+
+export default <T, TJoin>(
+  separator: TJoin,
+  arrays: Array<T[]>,
+): (T | TJoin)[] => withKey(flatMap)(
+  (arr: T[], i: number) => {
+    if (i < arrays.length - 1) {
+      return [...arr, separator];
+    }
+    return arr;
+  },
+  arrays,
+);
